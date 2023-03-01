31703000e894f740c3eb440decc2f5a2f866d025
lazizern <laziz.ibodullaev.82@gmail.com>
2023-03-01 12:05:23 -0500

    1st Commit

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.cydeo.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = " @buttons",
+        publish = true //generating a report with public link
+)
+
+public class CukesRunner {
+
+
+}
